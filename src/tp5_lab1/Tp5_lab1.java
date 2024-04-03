783cae146134e744d8469e958fc9203e368c79e9
Valentin Stieger <stiegervalentin123@gmail.com>
2024-04-03 19:32:41 -0300

    Initial commit

---
+++ src/tp5_lab1/Tp5_lab1.java
@@ -0,0 +1,12 @@
+package tp5_lab1;
+
+public class Tp5_lab1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
